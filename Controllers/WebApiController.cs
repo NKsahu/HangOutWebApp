707ee0d48affb6b23894e932ab61d4f8ba7906a1
957501deepak@gmail.com <957501deepak@gmail.com>
2019-10-31 22:55:08 -0700

    Api Controller Added

---
+++ Controllers/WebApiController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using HangOut.Models;
+using Newtonsoft.Json.Linq;
+
+namespace HangOut.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        public JObject GetLogin(string ObjLogin)
+        {
+
+            vw_HG_UsersDetails Objuser = Newtonsoft.Json.JsonConvert.DeserializeObject<vw_HG_UsersDetails>(ObjLogin);
+
+            vw_HG_UsersDetails LoginExist = Objuser.Checkvw_HG_UsersDetails();
+
+            return new JObject(LoginExist);
+
+        }
+
+    }
+}
