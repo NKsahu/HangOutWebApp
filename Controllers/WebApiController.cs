5153d741eb538c4564dc94652eb74aeb25881304
957501deepak@gmail.com <957501deepak@gmail.com>
2019-11-01 00:08:46 -0700

    UserDetail Save Method Added

--- Controllers/WebApiController.cs
+++ Controllers/WebApiController.cs
@@ -6,14 +6,34 @@ namespace HangOut.Controllers
 {
     public class WebApiController : ApiController
     {
-        public JObject GetLogin(string ObjLogin)
+        public JObject GetLogin(string Obj)
         {
 
-            vw_HG_UsersDetails Objuser = Newtonsoft.Json.JsonConvert.DeserializeObject<vw_HG_UsersDetails>(ObjLogin);
+            vw_HG_UsersDetails Objuser = Newtonsoft.Json.JsonConvert.DeserializeObject<vw_HG_UsersDetails>(Obj);
 
             vw_HG_UsersDetails LoginExist = Objuser.Checkvw_HG_UsersDetails();
 
             return new JObject(LoginExist);
+
+        }
+
+        public JObject GetRegistration(string Obj)
+        {
+
+            vw_HG_UsersDetails Objuser = Newtonsoft.Json.JsonConvert.DeserializeObject<vw_HG_UsersDetails>(Obj);
+
+            vw_HG_UsersDetails Exist = Objuser.Checkvw_HG_UsersDetails();
+            if(Exist!=null)
+            {
+                Objuser = null;
+            }else
+            {
+
+                Objuser.UserCode = Objuser.save();
+            }
+
+
+            return new JObject(Objuser);
 
         }
 
