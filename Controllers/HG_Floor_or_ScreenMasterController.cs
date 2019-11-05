2b8a9fc13d73bf2b87626c6a17ce668a0ded940d
957501deepak@gmail.com <957501deepak@gmail.com>
2019-11-05 13:22:43 +0530

    made two class and add jquery link

---
+++ Controllers/HG_Floor_or_ScreenMasterController.cs
@@ -0,0 +1,42 @@
+﻿using HangOut.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HangOut.Controllers
+{
+    public class HG_Floor_or_ScreenMasterController : Controller
+    {
+        // GET: HG_Floor_or_ScreenMaster
+        public ActionResult Index()
+        {
+            HG_Floor_or_ScreenMaster Objfloor = new HG_Floor_or_ScreenMaster();
+            List<HG_Floor_or_ScreenMaster> listfloor = Objfloor.GetAll();
+            return View(listfloor);
+        }
+        public ActionResult CreateEdit( int  ID)
+        {
+            HG_Floor_or_ScreenMaster Objfloor = new HG_Floor_or_ScreenMaster();
+            if(ID>0)
+            {
+                Objfloor = Objfloor.GetOne(ID);
+            }
+            return View(Objfloor);
+        }
+        [HttpPost]
+        public ActionResult CreateEdit(HG_Floor_or_ScreenMaster Objfloor)
+        {
+            int i = Objfloor.save();
+            if (i > 0)
+                return RedirectToAction("Index");
+            return RedirectToAction("Error");
+            
+        }
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/HG_FloorSide_or_RowName.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace HangOut.Models
+{
+    public class HG_FloorSide_or_RowName
+    {
+        public int  ID { get; set; }
+        public string FloorSide_or_RowName { get; set; }
+        public int RowSize { get; set; }
+        
+
+        public int save()
+        {
+            int Row = 0;
+            SqlConnection Con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Con"].ToString());
+            try
+            {
+                Con.Open();
+                SqlCommand cmd = null;
+                string Query = "";
+                if (this. ID == 0)
+
+                    Query = "Insert into  HG_FloorSide_or_RowName  values(@FloorSide_or_RowName,@RowSize); SELECT SCOPE_IDENTITY();";
+
+                else
+
+                    Query = "update  HG_FloorSide_or_RowName set  FloorSide_or_RowName=@FloorSide_or_RowName,RowSize=@RoeSize where ID=@ID";
+
+                cmd = new SqlCommand(Query, Con);
+                cmd.Parameters.AddWithValue("@Floor_or_ScreenID", this. ID);
+                cmd.Parameters.AddWithValue("@FloorSide_or_RowName", this.FloorSide_or_RowName);
+                cmd.Parameters.AddWithValue("@RowSize", this.RowSize);
+                if (this. ID == 0)
+                    Row = System.Convert.ToInt32(cmd.ExecuteScalar());
+                else if (cmd.ExecuteNonQuery() > 0)
+                {
+                    Row = this. ID;
+                }
+
+            }
+            catch (Exception e) { e.ToString(); }
+            finally { Con.Close(); }
+            return Row;
+        }
+        public List<HG_FloorSide_or_RowName> GetAll()
+        {
+            SqlConnection Con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Con"].ToString());
+            Con.Open();
+            SqlCommand cmd = null;
+            SqlDataReader SDR = null;
+            List<HG_FloorSide_or_RowName> ListTmp = new List<HG_FloorSide_or_RowName>();
+
+            try
+            {
+                string Query = "SELECT * FROM  HG_FloorSide_or_RowName ORDER BY Floor_or_ScreenID DESC";
+                cmd = new SqlCommand(Query, Con);
+                SDR = cmd.ExecuteReader();
+                while (SDR.Read())
+                {
+                    HG_FloorSide_or_RowName ObjTmp = new HG_FloorSide_or_RowName();
+                    ObjTmp. ID = SDR.GetInt32(0);
+                    ObjTmp.FloorSide_or_RowName = SDR.GetString(1);
+                    ObjTmp.RowSize = SDR.GetInt32(2);
+                    
+
+
+                    ListTmp.Add(ObjTmp);
+                }
+            }
+            catch (System.Exception e) { e.ToString(); }
+            finally { Con.Close(); }
+
+            return (ListTmp);
+        }
+        public HG_FloorSide_or_RowName GetOne(int Floor_or_ScreenID)
+        {
+            SqlConnection Con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["Con"].ToString());
+            Con.Open();
+            SqlCommand cmd = null;
+            SqlDataReader SDR = null;
+            HG_FloorSide_or_RowName ObjTmp = new HG_FloorSide_or_RowName();
+
+            try
+            {
+                string Query = "SELECT * FROM  HG_FloorSide_or_RowName where RantalID=@RantalID";
+                cmd = new SqlCommand(Query, Con);
+                cmd.Parameters.AddWithValue("@Floor_or_ScreenID", Floor_or_ScreenID);
+                SDR = cmd.ExecuteReader();
+                while (SDR.Read())
+                {
+                    ObjTmp.ID = SDR.GetInt32(0);
+                    ObjTmp.FloorSide_or_RowName = SDR.GetString(1);
+                    ObjTmp.RowSize = SDR.GetInt32(2);
+                }
+            }
+            catch (System.Exception e)
+            { e.ToString(); }
+
+            finally { Con.Close(); }
+
+            return (ObjTmp);
+        }
+
+    }
+}
