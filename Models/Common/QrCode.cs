ceb8fc1d1d33dcef06c4a46b652629ff33e2af25
harinaveen01 <harinaveen01@gmail.com>
2019-11-06 12:04:28 +0530

    QRImgTorR fOLDER aDDED

--- Models/Common/QrCode.cs
+++ Models/Common/QrCode.cs
@@ -1,11 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Drawing;
+using ZXing;
+using System.IO;
+using System.Drawing.Imaging;
 using System.Web;
 
 namespace HangOut.Models.Common
 {
     public class QrCode
     {
-    }
+        public static bool GenerateQr(int QrId,string ShowLabel = "")
+        {
+            var QCwriter = new BarcodeWriter();
+            QCwriter.Format = BarcodeFormat.QR_CODE;
+            var result = QCwriter.Write(QrId.ToString());
+            string path =HttpContext.Current.Server.MapPath("~/QRImgTorR/"+QrId.ToString()+".jpg");
+            var barcodeBitmap = new Bitmap(result);
+            using (var memoryStream = new MemoryStream())
+            {
+                
+            }
+
+            
+
+        
+
+            return false;
+        }
+
+   
+
+}
 }
