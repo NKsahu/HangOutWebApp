7748f75f42c0647cb1b269c97e699f4230e00b7e
harinaveen01 <harinaveen01@gmail.com>
2019-11-08 15:00:54 +0530

    City Index View Added

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -1,14 +1,19 @@
-﻿
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
-
+using HangOut.Models.Common;
 namespace HangOut.Controllers
 {
     public class CityController : Controller
     {
         // GET: City
-        public ActionResult Index()
+        public ActionResult Index(int Type,int StateCode=0)
         {
-            return View();
+            List<City> cities = new List<City>();
+            if (Type != 1)
+            {
+                cities = new City().GetAllByState(StateCode);
+            }
+            return View(cities);
         }
         public ActionResult FilterCity()
         {
