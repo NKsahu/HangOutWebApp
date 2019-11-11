7e6094acfae6512447bfd60b8f9a286b731d98ad
harinaveen01 <harinaveen01@gmail.com>
2019-11-11 15:25:24 +0530

    modifeid

--- Controllers/WebApiController.cs
+++ Controllers/WebApiController.cs
@@ -167,10 +167,10 @@ namespace HangOut.Controllers
         {
             return JArray.FromObject( Cart.List.FindAll(x => x.CID == int.Parse(CID)));
         }
-        public JObject StateList()
+        public JArray StateList()
         {
             List<State> StateList = new State().GetAll();
-            return JObject.FromObject(StateList);
+            return JArray.FromObject(StateList);
         }
          [HttpPost]
         public JArray CityListByStateId(int StateId)
