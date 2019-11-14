0b26d66a8054b2b518f86e0964191010ed3bc81f
harinaveen01 <harinaveen01@gmail.com>
2019-11-14 17:16:25 +0530

    Common Module select class added

---
+++ Models/Common/Select2Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HangOut.Models.Common
+{
+    public class Select2Result
+    {
+        public string text { get; set; }
+        public string id { get; set; }
+        public string Prop1 { get; set; }
+        public string Prop2 { get; set; }
+
+    }
+}
