ab0b1dc208f5eac08fe70141f1a94bffec8153c3
957501deepak@gmail.com <957501deepak@gmail.com>
2019-11-16 12:57:51 +0530

    Some Changes

--- Models/Common/Settings.cs
+++ Models/Common/Settings.cs
@@ -117,7 +117,7 @@ namespace HangOut.Models.Common
             Settings ObjTemp = new Settings();
             try
             {
-                string Query = "SELECT * FROM  Settings where KeyName=" + keyname;
+                string Query = "SELECT * FROM  Settings where KeyName=" + SettingId.ToString();
                 cmd = new SqlCommand(Query, Con);
                 SDR = cmd.ExecuteReader();
                 while (SDR.Read())
