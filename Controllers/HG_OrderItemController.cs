8a748c283580c4f83a9e45165bffc6a894ae70df
957501deepak@gmail.com <957501deepak@gmail.com>
2019-11-18 13:38:29 +0530

    add oder list popup

---
+++ Controllers/HG_OrderItemController.cs
@@ -0,0 +1,21 @@
+﻿using HangOut.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HangOut.Controllers
+{
+    public class HG_OrderItemController : Controller
+    {
+        // GET: HG_OrderItem
+        public ActionResult Index( Int64 OID)
+        {
+            HG_OrderItem Objorder = new HG_OrderItem();
+            List<HG_OrderItem> listorder = Objorder.GetAll(OID);
+
+            return View(listorder);
+        }
+    }
+}
