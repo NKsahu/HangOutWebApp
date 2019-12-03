73a7add6d04c0d875cd6da8dcfe2ee31a7b292ba
harinaveen01 <harinaveen01@gmail.com>
2019-12-03 16:43:13 +0530

    HG_orders getall modifed

--- Models/HG_Orders.cs
+++ Models/HG_Orders.cs
@@ -175,11 +175,12 @@ namespace HangOut.Models
             System.Data.SqlClient.SqlCommand cmd = null;
             System.Data.SqlClient.SqlDataReader SDR = null;
             List<HG_Orders> ListTmp = new List<HG_Orders>();
+            var theDate = new DateTime(Todate.Year, Todate.Month, Todate.Day, 23, 59, 00);
             HG_Orders ObjTmp = null;
             DBCon Obj = new DBCon();
             try
             {
-                string Query = "SELECT * FROM HG_ORDERS WHERE Create_Date between '" + Formdate.ToString("MM/dd/yyyy")+"' and '"+Todate.ToString("MM/dd/yyyy HH:mm:ss")+"' ORDER BY OID DESC";
+                string Query = "SELECT * FROM HG_ORDERS WHERE Create_Date between '" + Formdate.ToString("yyyy/MM/dd")+"' and '"+ theDate.ToString("yyyy/MM/dd")+"' ORDER BY OID DESC";
                 cmd = new SqlCommand(Query, Obj.Con);
                 SDR = cmd.ExecuteReader();
                 while (SDR.Read())
