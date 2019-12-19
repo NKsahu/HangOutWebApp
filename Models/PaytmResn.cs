5f62dc47d74c813a946ba7eab9bc0d87d160b23d
harinaveen01 <harinaveen01@gmail.com>
2019-12-19 16:10:36 +0530

    Add class Paytm Response

---
+++ Models/PaytmResn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HangOut.Models
+{
+    public class PaytmResn
+    {
+
+      //  [ID]
+      //,[OID]
+      //,[OIDKey]
+      //,[TxtId]
+      //,[TxtSts]
+      //,[TxtDate]
+      //,[CID]
+      //,[PaytmResponse]
+
+
+
+
+
+    }
+}
