577f326bfa487d99c668d460caf5bcc1431e7163
harinaveen01 <harinaveen01@gmail.com>
2019-12-30 12:40:38 +0530

    ReadExlFile class added and Readexcel DataSet Dll added

---
+++ Models/Common/ReadExl.cs
@@ -0,0 +1,36 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace HangOut.Models.Common
+{
+    public class ReadExl
+    {
+
+        public DataTable convertExcelToDatatable(string fpath)
+        {
+            //var fileName = string.Format("{0}\\fileNameHere", Directory.GetCurrentDirectory());
+            var connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=Excel 8.0;", fpath);
+
+            var adapter = new OleDbDataAdapter("select * from [Sheet1$]", connectionString);
+            var ds = new DataSet();
+
+            adapter.Fill(ds, "anyNameHere");
+
+            DataTable data = ds.Tables["anyNameHere"];
+            return data;
+        }
+
+        public DataTable ReadExcelFileDT(string fpath)
+        {
+            fpath = AppDomain.CurrentDomain.BaseDirectory + fpath.Replace("/", @"\");
+            return convertExcelToDatatable(fpath);
+        }
+
+    }
+}
