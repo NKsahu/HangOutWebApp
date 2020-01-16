0b618db4556f9fbcddf51f2ff9ed25da5bddbc7d
harinaveen01 <harinaveen01@gmail.com>
2020-01-16 13:54:06 +0530

    dfgfdg

--- Assets/PushNotication.js
+++ Assets/PushNotication.js
@@ -32,3 +32,19 @@ messaging.onMessage(function (payload) {
     console.log("Message received. ", payload);
    // NotisElem.innerHTML = NotisElem.innerHTML + JSON.stringify(payload)
 });
+self.addEventListener('push', function (event) {
+    console.log('Received a push message', event);
+
+    var title = 'Yay a message.';
+    var body = 'We have received a push message.';
+    var icon = '/images/icon-192x192.png';
+    var tag = 'simple-push-demo-notification-tag';
+
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: tag
+        })
+    );
+});
