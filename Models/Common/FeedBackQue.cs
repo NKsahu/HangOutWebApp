3afe1deb8de987fb206209b31c782ca03fc27b5d
harinaveen01 <harinaveen01@gmail.com>
2020-01-31 13:30:48 +0530

    rtrrt

--- Models/Common/FeedBackQue.cs
+++ Models/Common/FeedBackQue.cs
@@ -7,5 +7,9 @@ namespace HangOut.Models.Common
 {
     public class FeedBackQue
     {
+       public int ID { get; set; }
+      public string Title { get; set; }
+      public bool Status { get; set; }
+      public int QuestionType { get; set; }
     }
 }
