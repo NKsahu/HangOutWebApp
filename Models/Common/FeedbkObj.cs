6fa3f09471eeed3ed239a802e9968911ad5d33ea
harinaveen01 <harinaveen01@gmail.com>
2020-01-31 13:27:47 +0530

    dfgfh

--- Models/Common/FeedbkObj.cs
+++ Models/Common/FeedbkObj.cs
@@ -7,5 +7,9 @@ namespace HangOut.Models.Common
 {
     public class FeedbkObj
     {
+        public int id { get; set; }
+        public string  Name { get; set; }
+        public int ObjectiveType { get; set; }
+        public int QuestionId { get; set; }
     }
 }
