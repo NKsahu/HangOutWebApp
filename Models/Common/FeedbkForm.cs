6c61a4470e9156b52de02199a774ac9e3e4b66f9
957501deepak@gmail.com <957501deepak@gmail.com>
2020-01-31 16:43:50 +0530

    add three class and made save and getall function

--- Models/Common/FeedbkForm.cs
+++ Models/Common/FeedbkForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -12,5 +13,74 @@ namespace HangOut.Models.Common
        public int  OrgId { get; set; }
        public bool Status { get; set; }
        public DateTime CreateDate { get; set; }
+        public int Save()
+        {
+            int Row = 0;
+            DBCon con = new DBCon();
+            SqlCommand cmd = null;
+
+            try
+            {
+                string Quary = "";
+
+                if (this.Id == 0)
+                {
+                    Quary = "Insert into FeedbkForm values (@Name,@OrgId,@Status,@CreateDate); SELECT SCOPE_IdENTITY();";
+                }
+                else
+                {
+                    Quary = "Update FeedbkForm Set Name=@Name,OrgId=@OrgId,Status=@Status,CreateDate=@CreateDate where Id=@Id";
+                }
+                cmd = new SqlCommand(Quary, con.Con);
+                cmd.Parameters.AddWithValue("@Id", this.Id);
+                cmd.Parameters.AddWithValue("@Name", this.Name);
+                cmd.Parameters.AddWithValue("@OrgId", this.OrgId);
+                cmd.Parameters.AddWithValue("@Status", this.Status);
+                cmd.Parameters.AddWithValue("@CreateDate", DateTime.Now);
+                if (this.Id == 0)
+                {
+                    Row = Convert.ToInt32(cmd.ExecuteScalar());
+                    this.Id = Row;
+                }
+                else
+                {
+                    Row = cmd.ExecuteNonQuery();
+                    //this.CategoryId = Row;
+                }
+
+            }
+            catch (Exception e) { e.ToString(); }
+            finally { cmd.Dispose(); con.Con.Close(); }
+            return Row;
+        }
+        public List<FeedbkForm> GetAll()
+        {
+            DBCon OBJCon = new DBCon();
+            SqlCommand cmd = null;
+            SqlDataReader SDR = null;
+            List<FeedbkForm> ListTmp = new List<FeedbkForm>();
+
+            try
+            {
+                string Query = "SELECT * FROM  FeedbkForm ORDER BY  Id DESC";
+                cmd = new SqlCommand(Query, OBJCon.Con);
+                SDR = cmd.ExecuteReader();
+                while (SDR.Read())
+                {
+                    FeedbkForm ObjTmp = new FeedbkForm();
+                    ObjTmp.Id = SDR.GetInt32(0);
+                    ObjTmp.Name = SDR.GetString(1);
+                    ObjTmp.OrgId = SDR.GetInt32(2);
+                    ObjTmp.Status = SDR.GetBoolean(3);
+                    ObjTmp.CreateDate = SDR.GetDateTime(4);
+
+                    ListTmp.Add(ObjTmp);
+                }
+            }
+            catch (System.Exception e) { e.ToString(); }
+            finally { cmd.Dispose(); ; OBJCon.Con.Close(); }
+
+            return (ListTmp);
+        }
     }
 }
--- Models/Common/FeedbkObj.cs
+++ Models/Common/FeedbkObj.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,75 @@ namespace HangOut.Models.Common
         public int id { get; set; }
         public string  Name { get; set; }
         public int ObjectiveType { get; set; }
-        public int QuestionId { get; set; }
+        public int QuestionId{ get; set; }
+        public int Save()
+        {
+            int Row = 0;
+            DBCon con = new DBCon();
+            SqlCommand cmd = null;
+
+            try
+            {
+                string Quary = "";
+
+                if (this.id == 0)
+                {
+                    Quary = "Insert into FeedbkObj values (@Name,@ObjectiveType,@QuestionId); SELECT SCOPE_idENTITY();";
+                }
+                else
+                {
+                    Quary = "Update FeedbkObj Set Name=@Name,ObjectiveType=@ObjectiveType,QuestionId=@QuestionId where id=@id";
+                }
+                cmd = new SqlCommand(Quary, con.Con);
+                cmd.Parameters.AddWithValue("@id", this.id);
+                cmd.Parameters.AddWithValue("@Name", this.Name);
+                cmd.Parameters.AddWithValue("@Orgid", this.ObjectiveType);
+                cmd.Parameters.AddWithValue("@Status", this.QuestionId);
+                cmd.Parameters.AddWithValue("@CreateDate", DateTime.Now);
+                if (this.id == 0)
+                {
+                    Row = Convert.ToInt32(cmd.ExecuteScalar());
+                    this.id = Row;
+                }
+                else
+                {
+                    Row = cmd.ExecuteNonQuery();
+                    //this.Categoryid = Row;
+                }
+
+            }
+            catch (Exception e) { e.ToString(); }
+            finally { cmd.Dispose(); con.Con.Close(); }
+            return Row;
+        }
+        public List<FeedbkObj> GetAll()
+        {
+            DBCon OBJCon = new DBCon();
+            SqlCommand cmd = null;
+            SqlDataReader SDR = null;
+            List<FeedbkObj> ListTmp = new List<FeedbkObj>();
+
+            try
+            {
+                string Query = "SELECT * FROM  FeedbkObj ORDER BY  id DESC";
+                cmd = new SqlCommand(Query, OBJCon.Con);
+                SDR = cmd.ExecuteReader();
+                while (SDR.Read())
+                {
+                    FeedbkObj ObjTmp = new FeedbkObj();
+                    ObjTmp.id = SDR.GetInt32(0);
+                    ObjTmp.Name = SDR.GetString(1);
+                    ObjTmp.ObjectiveType = SDR.GetInt32(2);
+                    ObjTmp.QuestionId = SDR.GetInt32   (3);
+                    
+
+                    ListTmp.Add(ObjTmp);
+                }
+            }
+            catch (System.Exception e) { e.ToString(); }
+            finally { cmd.Dispose(); ; OBJCon.Con.Close(); }
+
+            return (ListTmp);
+        }
     }
 }
