59903389af02a8da8022891c2e72c950a0d2eb3f
harinaveen01 <harinaveen01@gmail.com>
2020-02-04 17:35:16 +0530

    feedbk added

---
+++ Models/Feedbk/FeedbkItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HangOut.Models.Feedbk
+{
+    public class FeedbkItem
+    {
+       public Int64  ItemID { get; set; }
+       public int Rating { get; set; }
+       public string Comment { get; set; }
+       public int FeedbkFormID { get; set; }
+       public int FeedBkID { get; set; }
+       public int  ResponseType { get; set; }
+       public DateTime CreateOn { get; set; }
+       public int CID { get; set; }
+
+    }
+}
