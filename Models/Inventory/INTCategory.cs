753606ecd375ce368fa9e8fe58fcbd4f5e9823d2
957501deepak@gmail.com <957501deepak@gmail.com>
2020-02-11 17:54:45 +0530

    mamla done on success fully

--- Models/Inventory/INTCategory.cs
+++ Models/Inventory/INTCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,65 @@ namespace HangOut.Models.Inventory
     {
         public int CatID { get; set; }
         public string Name { get; set; }
-        
+
+        public int Save()
+        {
+            int Row = 0;
+            DBCon con = new DBCon();
+            SqlCommand cmd = null;
+            try
+            {
+                string Quary = "";
+                if (this.CatID == 0)
+                {
+                    Quary = "Insert Into INTCategory Values (@Name);SELECT SCOPE_IDENTITY();";
+                }
+                else
+                {
+                    Quary = "Update INTCategory Set Name=@Name where CatID=@CatID";
+                }
+                cmd = new SqlCommand(Quary, con.Con);
+                cmd.Parameters.AddWithValue("@CatID", this.CatID);
+                cmd.Parameters.AddWithValue("@Name", this.Name);
+                if (this.CatID == 0)
+                {
+                    Row = Convert.ToInt32(cmd.ExecuteScalar());
+                    this.CatID = Row;
+                }
+                else
+                {
+                    Row = cmd.ExecuteNonQuery();
+                    //this.CategoryID = Row;
+                }
+
+            }
+            catch (Exception e) { e.ToString(); }
+            finally { cmd.Dispose(); con.Con.Close(); }
+            return Row;
+
+        }
+        public static List<INTCategory> GetAll()
+        {
+            DBCon con = new DBCon();
+            SqlCommand cmd = null;
+            SqlDataReader SDR = null;
+            List<INTCategory> listintcat = new List<INTCategory>();
+            try
+            {
+                string Quary = "Select * from INTCategory where ORDER BY CatID DESC";
+                cmd = new SqlCommand(Quary, con.Con);
+                SDR = cmd.ExecuteReader();
+
+                while (SDR.Read())
+                {
+                    INTCategory OBJINT = new INTCategory();
+                    OBJINT.CatID = SDR.GetInt32(0);
+                    OBJINT.Name = SDR.GetString(1);
+                }
+            }
+            catch (Exception e) { e.ToString(); }
+            finally { cmd.Dispose(); con.Con.Close(); }
+            return (listintcat);
+        }
     }
 }
--- Models/Inventory/INTGSTBL.cs
+++ Models/Inventory/INTGSTBL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -13,5 +14,72 @@ namespace HangOut.Models.Inventory
         public int Type { get; set; }
         public int Unit { get; set; }
         public double Qty { get; set; }
+        public int Save()
+        {
+            int Row = 0;
+            DBCon con = new DBCon();
+            SqlCommand cmd = null;
+            try
+            {
+                string Quary = "";
+                if (this.CatID == 0)
+                {
+                    Quary = "Insert Into INTGSTBL Values (@CatID,@Name,@Type,@Unit,@Qty);SELECT SCOPE_IDENTITY();";
+                }
+                else
+                {
+                    Quary = "Update INTGSTBL Set CatID=@CatID,Name=@Name,Type=@Type,Unit=@unit,Qty=@Qty where GSID=@GSID";
+                }
+                cmd = new SqlCommand(Quary, con.Con);
+                cmd.Parameters.AddWithValue("@GSID", this.GSID);
+                cmd.Parameters.AddWithValue("@CatID", this.CatID);
+                cmd.Parameters.AddWithValue("@Name", this.Name);
+                cmd.Parameters.AddWithValue("@Type", this.Type);
+                cmd.Parameters.AddWithValue("@Unit", this.Unit);
+                cmd.Parameters.AddWithValue("@Qty", this.Qty);
+                if (this.CatID == 0)
+                {
+                    Row = Convert.ToInt32(cmd.ExecuteScalar());
+                    this.CatID = Row;
+                }
+                else
+                {
+                    Row = cmd.ExecuteNonQuery();
+                    //this.CategoryID = Row;
+                }
+
+            }
+            catch (Exception e) { e.ToString(); }
+            finally { cmd.Dispose(); con.Con.Close(); }
+            return Row;
+
+        }
+        public static List<INTGSTBL> GetAll()
+        {
+            DBCon con = new DBCon();
+            SqlCommand cmd = null;
+            SqlDataReader SDR = null;
+            List<INTGSTBL> listintcat = new List<INTGSTBL>();
+            try
+            {
+                string Quary = "Select * from INTGSTBL where ORDER BY GSID DESC";
+                cmd = new SqlCommand(Quary, con.Con);
+                SDR = cmd.ExecuteReader();
+
+                while (SDR.Read())
+                {
+                    INTGSTBL OBJINT = new INTGSTBL();
+                    OBJINT.GSID = SDR.GetInt32(0);
+                    OBJINT.CatID = SDR.GetInt32(1);
+                    OBJINT.Name = SDR.GetString(2);
+                    OBJINT.Type = SDR.GetInt32(3);
+                    OBJINT.Unit = SDR.GetInt32(4);
+                    OBJINT.Qty = SDR.GetDouble(5);
+                }
+            }
+            catch (Exception e) { e.ToString(); }
+            finally { cmd.Dispose(); con.Con.Close(); }
+            return (listintcat);
+        }
     }
 }
