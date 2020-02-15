ae1ee2fef6927d583edc626b2b95d1d7bdb0a68e
harinaveen01 <harinaveen01@gmail.com>
2020-02-15 16:03:32 +0530

    orddiscont

--- Models/OrdDiscntChrge.cs
+++ Models/OrdDiscntChrge.cs
@@ -7,5 +7,12 @@ namespace HangOut.Models
 {
     public class OrdDiscntChrge
     {
+      public int   ID { get; set; }
+      public string Title { get; set; }
+      public Int64 OID { get; set; }
+      public int Type { get; set; }// 1 discount, 2 charge
+      public double TaxOrAmt { get; set; }
+      
+
     }
 }
