5b1a0a2800097d2a0fcbafda6d929dc9583812db
harinaveen01 <harinaveen01@gmail.com>
2020-02-27 11:07:20 +0530

    no changes

--- Controllers/Inventory/INTGSTBLController.cs
+++ Controllers/Inventory/INTGSTBLController.cs
@@ -58,8 +58,8 @@ namespace HangOut.Controllers.Inventory
             INTItems iNTItems = new INTItems();
             return View(iNTItems);
         }
-        public JsonResult GetAmt(int PRID)
-        {
+       // public JsonResult GetAmt(int PRID)
+       // {
             //List<Rental> ListRentals = new Rental().GetAll();
             //Rental OnjRentals = ListRentals.Find(x => x.RantalID == PRID);
             //List<RoomTable> ListRooms = new RoomTable().GetAll();
@@ -71,6 +71,6 @@ namespace HangOut.Controllers.Inventory
             //List<Login> listlogin = new Login().GetAll();
             //Login OBJLogin = listlogin.Find(x => x.ID == OnjRentals.ID);
             //return Json(new { RID = ObjRooms.RID, RentAgreementFor = OnjRentals.RentAgreementFor, RoomNumber = ObjRooms.RoomNumber, RantAmt = ObjRooms.RantAmt, DDID = ObjRooms.DDID, ID = OBJLogin.ID }, JsonRequestBehavior.AllowGet);
-        }
+        //}
     }
 }
