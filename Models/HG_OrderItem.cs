73e4623ab71d4ed8e2002028224968bfb7023ca4
harinaveen01 <harinaveen01@gmail.com>
2020-03-02 11:11:58 +0530

    orderitem GetTaxbleAmt added

--- Models/HG_OrderItem.cs
+++ Models/HG_OrderItem.cs
@@ -41,11 +41,11 @@ namespace HangOut.Models
             DBCon Obj = new DBCon();
             try
             {
-                if (this.OIID == 0) { 
+                if (this.OIID == 0) {
                     cmd = new System.Data.SqlClient.SqlCommand("INSERT INTO HG_ORDERITEM (FID,Price,Count,Qty,OID,Deleted,Status,OrderDate,UpdatedBy,UpdationDate,TickedNo,ChefSeenBy,OrgId,OrdById,TaxInItm,CostPrice) VALUES (@FID,@Price,@Count,@Qty,@OID,@Deleted,@Status,@OrderDate,@UpdatedBy,@UpdationDate,@TickedNo,@ChefSeenBy,@OrgId,@OrdById,@TaxInItm,@CostPrice);select SCOPE_IDENTITY();", Obj.Con);
                     cmd.Parameters.AddWithValue("@OrdById", this.OrdById);
                     cmd.Parameters.AddWithValue("@TaxInItm", this.TaxInItm);
-            }
+                }
                 else
                 {
                     cmd = new System.Data.SqlClient.SqlCommand("UPDATE HG_ORDERITEM SET FID=@FID,Price=@Price,Count=@Count,Qty=@Qty,OID=@OID,Deleted=@Deleted,Status=@Status,UpdatedBy=@UpdatedBy,UpdationDate=@UpdationDate,TickedNo=@TickedNo,ChefSeenBy=@ChefSeenBy,OrgId=@OrgId,CostPrice=@CostPrice where OIID=@OIID", Obj.Con);
@@ -83,7 +83,7 @@ namespace HangOut.Models
             return Row;
         }
 
-        public List<HG_OrderItem> GetAll(Int64 OID=0)
+        public List<HG_OrderItem> GetAll(Int64 OID = 0)
         {
             var CurrOrgID = HttpContext.Current.Request.Cookies["UserInfo"];
             System.Data.SqlClient.SqlCommand cmd = null;
@@ -97,72 +97,9 @@ namespace HangOut.Models
                 {
                     Query = "SELECT * FROM HG_ORDERITEM WHERE OID=" + OID.ToString() + " and Deleted=0 ";
                 }
-                else if(int.Parse(CurrOrgID["OrgId"]) > 0)
+                else if (int.Parse(CurrOrgID["OrgId"]) > 0)
                 {
-                 Query = "SELECT * FROM HG_ORDERITEM WHERE OrgId=" + CurrOrgID["OrgId"] + " and Deleted=0 ";
-                }
-                cmd = new System.Data.SqlClient.SqlCommand(Query, Obj.Con);
-                SDR = cmd.ExecuteReader();
-                while (SDR.Read())
-                {
-                    HG_OrderItem ObjTmp = new HG_OrderItem();
-                    ObjTmp.OIID = SDR.GetInt64(0);
-                    ObjTmp.FID = SDR.GetInt64(1);
-                    ObjTmp.Price = SDR.GetDouble(2);
-                    ObjTmp.Count = SDR.GetInt32(3);
-                    ObjTmp.Qty = SDR.GetString(4);
-                    ObjTmp.OID = SDR.GetInt64(5);
-                    ObjTmp.Status = SDR.GetInt32(7);
-                    ObjTmp.OrderDate =SDR.GetDateTime(8);
-                    ObjTmp.UpdatedBy =SDR.GetInt32(9);
-                    ObjTmp.UpdationDate =SDR.GetDateTime(10);
-                    ObjTmp.TickedNo = SDR.GetInt32(11);
-                    ObjTmp.ChefSeenBy = SDR.GetInt32(12);
-                    ObjTmp.OrgId = SDR.GetInt32(13);
-                    ObjTmp.OrdById = SDR.GetInt64(14);
-                    ObjTmp.TaxInItm = SDR.GetDouble(15);
-                    ObjTmp.CostPrice = SDR.GetDouble(16);
-                    ListTmp.Add(ObjTmp);
-                }
-            }
-            catch (System.Exception e) { e.ToString(); }
-            finally { cmd.Dispose(); SDR.Close(); Obj.Con.Close(); Obj.Con.Dispose(); Obj.Con = null; }
-            return (ListTmp);
-        }
-        public List<HG_OrderItem> GetAllByOrg(int OrgId,int ChefId=0,int ItemStatus=0,bool TodayOnly=false,string Status=null)
-        {
-            System.Data.SqlClient.SqlCommand cmd = null;
-            System.Data.SqlClient.SqlDataReader SDR = null;
-            List<HG_OrderItem> ListTmp = new List<HG_OrderItem>();
-            DBCon Obj = new DBCon();
-            try
-            {
-                string Query = "";
-                if (OrgId > 0)
-                {
-                    Query=  "SELECT * FROM HG_ORDERITEM WHERE OrgId=" + OrgId.ToString() + "";
-                }
-                else if (OrgId <=0)//super admin && postpaid-admin
-                {
-                    Query = "SELECT * FROM HG_ORDERITEM WHERE OrgId>0";
-                }
-                if (ChefId > 0)
-                {
-                     Query+= " and (ChefSeenBy="+ChefId.ToString()+ " or ChefSeenBy=0)";
-                }
-                if (ItemStatus > 0)
-                {
-                    Query += " and Status=" + ItemStatus.ToString();
-                }
-                if (TodayOnly)
-                {
-                    var Formdate = DateTime.Now;
-                    var theDate = new DateTime(Formdate.Year, Formdate.Month, Formdate.Day, 23, 59, 00);
-                    Query = "SELECT * FROM HG_ORDERITEM WHERE OrderDate between '" + Formdate.ToString("MM/dd/yyyy") + "' and '" + theDate.ToString("MM/dd/yyyy HH:mm:ss") + "' and OrgId="+OrgId.ToString()+"";
-                }
-                if (Status != null &&ItemStatus==0)
-                {
-                    Query += " and "+Status;
+                    Query = "SELECT * FROM HG_ORDERITEM WHERE OrgId=" + CurrOrgID["OrgId"] + " and Deleted=0 ";
                 }
                 cmd = new System.Data.SqlClient.SqlCommand(Query, Obj.Con);
                 SDR = cmd.ExecuteReader();
@@ -192,11 +129,74 @@ namespace HangOut.Models
             finally { cmd.Dispose(); SDR.Close(); Obj.Con.Close(); Obj.Con.Dispose(); Obj.Con = null; }
             return (ListTmp);
         }
-        public HG_OrderItem GetOne(Int64 OIID=0)
+        public List<HG_OrderItem> GetAllByOrg(int OrgId, int ChefId = 0, int ItemStatus = 0, bool TodayOnly = false, string Status = null)
         {
             System.Data.SqlClient.SqlCommand cmd = null;
             System.Data.SqlClient.SqlDataReader SDR = null;
-           HG_OrderItem ObjTmp = new HG_OrderItem();
+            List<HG_OrderItem> ListTmp = new List<HG_OrderItem>();
+            DBCon Obj = new DBCon();
+            try
+            {
+                string Query = "";
+                if (OrgId > 0)
+                {
+                    Query = "SELECT * FROM HG_ORDERITEM WHERE OrgId=" + OrgId.ToString() + "";
+                }
+                else if (OrgId <= 0)//super admin && postpaid-admin
+                {
+                    Query = "SELECT * FROM HG_ORDERITEM WHERE OrgId>0";
+                }
+                if (ChefId > 0)
+                {
+                    Query += " and (ChefSeenBy=" + ChefId.ToString() + " or ChefSeenBy=0)";
+                }
+                if (ItemStatus > 0)
+                {
+                    Query += " and Status=" + ItemStatus.ToString();
+                }
+                if (TodayOnly)
+                {
+                    var Formdate = DateTime.Now;
+                    var theDate = new DateTime(Formdate.Year, Formdate.Month, Formdate.Day, 23, 59, 00);
+                    Query = "SELECT * FROM HG_ORDERITEM WHERE OrderDate between '" + Formdate.ToString("MM/dd/yyyy") + "' and '" + theDate.ToString("MM/dd/yyyy HH:mm:ss") + "' and OrgId=" + OrgId.ToString() + "";
+                }
+                if (Status != null && ItemStatus == 0)
+                {
+                    Query += " and " + Status;
+                }
+                cmd = new System.Data.SqlClient.SqlCommand(Query, Obj.Con);
+                SDR = cmd.ExecuteReader();
+                while (SDR.Read())
+                {
+                    HG_OrderItem ObjTmp = new HG_OrderItem();
+                    ObjTmp.OIID = SDR.GetInt64(0);
+                    ObjTmp.FID = SDR.GetInt64(1);
+                    ObjTmp.Price = SDR.GetDouble(2);
+                    ObjTmp.Count = SDR.GetInt32(3);
+                    ObjTmp.Qty = SDR.GetString(4);
+                    ObjTmp.OID = SDR.GetInt64(5);
+                    ObjTmp.Status = SDR.GetInt32(7);
+                    ObjTmp.OrderDate = SDR.GetDateTime(8);
+                    ObjTmp.UpdatedBy = SDR.GetInt32(9);
+                    ObjTmp.UpdationDate = SDR.GetDateTime(10);
+                    ObjTmp.TickedNo = SDR.GetInt32(11);
+                    ObjTmp.ChefSeenBy = SDR.GetInt32(12);
+                    ObjTmp.OrgId = SDR.GetInt32(13);
+                    ObjTmp.OrdById = SDR.GetInt64(14);
+                    ObjTmp.TaxInItm = SDR.GetDouble(15);
+                    ObjTmp.CostPrice = SDR.GetDouble(16);
+                    ListTmp.Add(ObjTmp);
+                }
+            }
+            catch (System.Exception e) { e.ToString(); }
+            finally { cmd.Dispose(); SDR.Close(); Obj.Con.Close(); Obj.Con.Dispose(); Obj.Con = null; }
+            return (ListTmp);
+        }
+        public HG_OrderItem GetOne(Int64 OIID = 0)
+        {
+            System.Data.SqlClient.SqlCommand cmd = null;
+            System.Data.SqlClient.SqlDataReader SDR = null;
+            HG_OrderItem ObjTmp = new HG_OrderItem();
             DBCon Obj = new DBCon();
             try
             {
@@ -209,19 +209,19 @@ namespace HangOut.Models
                 SDR = cmd.ExecuteReader();
                 while (SDR.Read())
                 {
-                     ObjTmp = new HG_OrderItem();
+                    ObjTmp = new HG_OrderItem();
                     ObjTmp.OIID = SDR.GetInt64(0);
                     ObjTmp.FID = SDR.GetInt64(1);
                     ObjTmp.Price = SDR.GetDouble(2);
                     ObjTmp.Count = SDR.GetInt32(3);
                     ObjTmp.Qty = SDR.GetString(4);
                     ObjTmp.OID = SDR.GetInt64(5);
-                    ObjTmp.Status =  SDR.GetInt32(7);
+                    ObjTmp.Status = SDR.GetInt32(7);
                     ObjTmp.OrderDate = SDR.GetDateTime(8);
-                    ObjTmp.UpdatedBy =  SDR.GetInt32(9);
+                    ObjTmp.UpdatedBy = SDR.GetInt32(9);
                     ObjTmp.UpdationDate = SDR.GetDateTime(10);
                     ObjTmp.TickedNo = SDR.GetInt32(11);
-                    ObjTmp.ChefSeenBy =  SDR.GetInt32(12);
+                    ObjTmp.ChefSeenBy = SDR.GetInt32(12);
                     ObjTmp.OrgId = SDR.GetInt32(13);
                     ObjTmp.OrdById = SDR.GetInt64(14);
                     ObjTmp.TaxInItm = SDR.GetDouble(15);
@@ -236,12 +236,12 @@ namespace HangOut.Models
         public static HG_OrderItem PaybleAmt(Int64 OID)
         {
             HG_OrderItem objOrdItm = new HG_OrderItem();
-            var OrderItms =new HG_OrderItem().GetAll(OID);
-            for(int i=0;i< OrderItms.Count; i++)
+            var OrderItms = new HG_OrderItem().GetAll(OID);
+            for (int i = 0; i < OrderItms.Count; i++)
             {
                 objOrdItm.CostPrice += OrderItms[i].Count * OrderItms[i].CostPrice;
-                objOrdItm.TaxInItm+= OrgType.TotalTax(OrderItms[i].CostPrice, OrderItms[i].TaxInItm, OrderItms[i].Count);
-                objOrdItm.Price+= OrderItms[i].Count * OrderItms[i].Price;
+                objOrdItm.TaxInItm += OrgType.TotalTax(OrderItms[i].CostPrice, OrderItms[i].TaxInItm, OrderItms[i].Count);
+                objOrdItm.Price += OrderItms[i].Count * OrderItms[i].Price;
             }
             return objOrdItm;
         }
@@ -263,9 +263,9 @@ namespace HangOut.Models
             string Query = "SELECT distinct TickedNo,OID FROM HG_OrderItem WHERE  (Status=1 or Status=2) and OID In(select OID from HG_Orders where  Status='1' ";
             int Count = 0;
             HG_OrganizationDetails ObjOrg = new HG_OrganizationDetails().GetOne(OrgId);
-            if (OrgId==0|| (ObjOrg.OrgID>0&& ObjOrg.PaymentType == 1))
+            if (OrgId == 0 || (ObjOrg.OrgID > 0 && ObjOrg.PaymentType == 1))
             {
-              Query = "SELECT distinct TickedNo,OID FROM HG_OrderItem WHERE  (Status=1 or Status=2) and OID In(select OID from HG_Orders where  Status='1' and PaymentStatus!= 0 ";
+                Query = "SELECT distinct TickedNo,OID FROM HG_OrderItem WHERE  (Status=1 or Status=2) and OID In(select OID from HG_Orders where  Status='1' and PaymentStatus!= 0 ";
             }
             if (OrgId > 0)
             {
@@ -275,7 +275,7 @@ namespace HangOut.Models
             {
                 Query += ")";
             }
-           
+
             System.Data.SqlClient.SqlCommand cmd = null;
             System.Data.SqlClient.SqlDataReader SDR = null;
             HG_OrderItem ObjTmp = new HG_OrderItem();
@@ -293,6 +293,10 @@ namespace HangOut.Models
             catch (System.Exception e) { e.ToString(); }
             finally { cmd.Dispose(); SDR.Close(); Obj.Con.Close(); Obj.Con.Dispose(); Obj.Con = null; }
             return Count;
+        }
+        public static double GetTaxbleAmt (double Amt, double Tax){
+          double  TaxableAmt = (Amt * Tax) / 100;
+            return TaxableAmt;
         }
     }
 }
