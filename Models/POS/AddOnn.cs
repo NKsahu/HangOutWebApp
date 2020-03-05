1c994b643a7494d3dd43f5a3e71c8d440714006c
harinaveen01 <harinaveen01@gmail.com>
2020-03-05 16:34:25 +0530

    Addon field added

--- Models/POS/AddOnn.cs
+++ Models/POS/AddOnn.cs
@@ -7,5 +7,10 @@ namespace HangOut.Models.POS
 {
     public class AddOnn
     {
+        public int TitleId { get; set; }
+        public string TemplateName { get; set; }
+        public string AddOnTitle {get;set;}
+        public int Min { get; set; }
+       public int Max { get; set; }
     }
 }
