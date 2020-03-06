0101024a568a5938216055b4977045dae8f3b9a3
harinaveen01 <harinaveen01@gmail.com>
2020-03-06 10:48:36 +0530

    Add view added in shared folder

--- Models/POS/AddOnn.cs
+++ Models/POS/AddOnn.cs
@@ -12,11 +12,19 @@ namespace HangOut.Models.POS
         public string AddOnTitle {get;set;}
         public int Min { get; set; }
        public int Max { get; set; }
-        public int CategoryId { get; set; }
+        public int AddonCatId { get; set; }// addon category id
       public  List< AddOnItems> AddOnItems { get; set; }
         public AddOnn()
         {
             AddOnItems = new List<AddOnItems>();
+        }
+    }
+    public class AddOns
+    {
+       public  List<AddOnn> AddonnList { get; set; }
+        public AddOns()
+        {
+            AddonnList = new List<AddOnn>();
         }
     }
 }
