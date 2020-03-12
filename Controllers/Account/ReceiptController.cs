fd6d504c510993048753057bcf96deb037f80dc1
Rahul Soni <sonir2233@gmail.com>
2020-03-12 18:03:45 +0530

    re

--- Controllers/Account/ReceiptController.cs
+++ Controllers/Account/ReceiptController.cs
@@ -35,12 +35,18 @@ namespace HangOut.Controllers.Account
             double LastBalance = 0.00;
             int ENo = 0;
             Receipt ReceiptEntry = new Receipt();
+            ReceiptEntry.Date = Onlinepayment[0].TxtDate.AddHours(-24);
+            ReceiptEntry.ReceiptType = "Rece";
+            ReceiptEntry.Amount = 0.00;
+            ReceiptEntry.Particular = "Opening Balance";
+            ReceiptEntry.Save();
 
             foreach (var OP in Onlinepayment)
             {
                 ReceiptEntry = new Receipt();
 
                 ReceiptEntry.Date = OP.TxtDate;
+                ReceiptEntry.ReceiptType = "Rece";
                 ReceiptEntry.Amount =Convert.ToDouble(OP.PaidAmount);
                 ReceiptEntry.Particular = "Online Payment of Order No." + OP.OID;
                 ENo = ENo + 1;
