bd84fcff6e55a3c9b7aeb67147f3d45f56ea2069
harinaveen01 <harinaveen01@gmail.com>
2020-03-13 11:54:21 +0530

    new Folder POS controller added

---
+++ Controllers/POS/ServingSizeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HangOut.Controllers.POS
+{
+    public class ServingSizeController : Controller
+    {
+        // GET: ServingSize
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreatEdit()
+        {
+            return View();
+        }
+    }
+}
