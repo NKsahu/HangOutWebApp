3720c2e2c157516652d095e53f65ea808d1595a7
harinaveen01 <harinaveen01@gmail.com>
2020-04-02 12:01:34 +0530

    Cashback controller added

---
+++ Controllers/Customer/CashBackController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HangOut.Controllers.Customer
+{
+    public class CashBackController : Controller
+    {
+        // GET: CashBack
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
